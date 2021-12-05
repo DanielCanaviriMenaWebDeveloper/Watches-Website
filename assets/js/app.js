010019d724474c4e76b67fee3ccdcafb95fd42fc
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-12-04 23:38:23 -0400

    Seccion Testimonials Finalizada para dispositivos moviles

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,3 +42,14 @@ function scrollHeader() {
     }
 }
 window.addEventListener('scroll', scrollHeader);
+
+/* Testimonial Swiper */
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: "true",
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
